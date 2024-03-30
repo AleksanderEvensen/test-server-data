a5bcb656330c2f87908dc693d191f12e44909358
Aleksander Evensen <ahse03@gmail.com>
2024-03-30 16:29:45 +0100

    Added a test resource

---
+++ resources/[test]/test-resource/fxmanifest.lua
@@ -0,0 +1,6 @@
+-- compatibility wrapper
+fx_version 'cerulean'
+game 'gta5'
+
+
+server_script 'sv_main.js'
---
+++ resources/[test]/test-resource/sv_main.js
@@ -0,0 +1,12 @@
+const message = `
+
+
+This is a message from the 'test-resource'
+
+The current branch is 'main'
+
+
+
+`;
+
+console.log(message);
